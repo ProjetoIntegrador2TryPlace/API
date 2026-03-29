a4de634be25b12e7610f5da783ba6e6312e940b4
pdro10 <elpedrorenan08@gmail.com>
2026-03-29 09:28:55 -0300

    Inicialização da estrutura do projeto

---
+++ src/main/kotlin/com/tryplace/tryplace/TryplaceApplication.kt
@@ -0,0 +1,11 @@
+package com.tryplace.tryplace
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TryplaceApplication
+
+fun main(args: Array<String>) {
+	runApplication<TryplaceApplication>(*args)
+}
---
+++ src/test/kotlin/com/tryplace/tryplace/TryplaceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tryplace.tryplace
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TryplaceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
