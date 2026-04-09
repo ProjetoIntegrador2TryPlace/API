c7eb7262a28ad468d54c59c120ce8c9817b57a7c
pdro10 <elpedrorenan08@gmail.com>
2026-04-09 19:08:41 -0300

    implementação do UserDetails no model para integração com o Spring Security

--- src/main/kotlin/com/tryplace/tryplace/model/LocadorModel.kt
+++ src/main/kotlin/com/tryplace/tryplace/model/LocadorModel.kt
@@ -7,6 +7,8 @@ import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Table
 import org.hibernate.annotations.CreationTimestamp
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
 import java.util.UUID
 import java.time.Instant
 
@@ -32,4 +34,20 @@ data class LocadorModel(
     @CreationTimestamp
     var criadoEm: Instant? = null
 
-)
+) : UserDetails {
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> = mutableListOf()
+
+    override fun getPassword(): String = senha
+
+    override fun getUsername(): String = email
+
+    override fun isAccountNonExpired() = true
+
+    override fun isAccountNonLocked() = true
+
+    override fun isCredentialsNonExpired() = true
+
+    override fun isEnabled() = true
+
+
+}
