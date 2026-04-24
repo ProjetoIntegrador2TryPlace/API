bdda33cc0c2162a5843d39917d3dc85c0431b3b8
JoseEric <joseeric2310@gmail.com>
2026-04-24 09:10:45 -0300

    feat: sprint 3

--- src/main/kotlin/com/tryplace/tryplace/configs/SecurityFilter.kt
+++ src/main/kotlin/com/tryplace/tryplace/configs/SecurityFilter.kt
@@ -16,17 +16,35 @@ class SecurityFilter(
     private val usuarioRepository: UsuarioRepository,
 ) : OncePerRequestFilter() {
 
-    override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) {
+    override fun shouldNotFilter(request: HttpServletRequest): Boolean {
+        val path = request.servletPath
+        return path.startsWith("/api/login")
+    }
+
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
         val tokenJWT = recuperarToken(request)
 
         if (tokenJWT != null) {
-            val subject = tokenService.getSubject(tokenJWT)
+            try {
+                val subject = tokenService.getSubject(tokenJWT)
 
-            val usuario = usuarioRepository.findByEmail(subject)
+                val usuario = usuarioRepository.findByEmail(subject)
 
-            if (usuario != null) {
-                val authentication = UsernamePasswordAuthenticationToken(usuario, null, usuario.authorities)
-                SecurityContextHolder.getContext().authentication = authentication
+                if (usuario != null) {
+                    val authentication = UsernamePasswordAuthenticationToken(
+                        usuario,
+                        null,
+                        usuario.authorities
+                    )
+                    SecurityContextHolder.getContext().authentication = authentication
+                }
+
+            } catch (e: Exception) {
+                println("🔥 Token inválido ou expirado: ${e.message}")
             }
         }
 
@@ -35,7 +53,7 @@ class SecurityFilter(
 
     private fun recuperarToken(request: HttpServletRequest): String? {
         val authorizationHeader = request.getHeader("Authorization")
-        if (authorizationHeader != null) {
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
             return authorizationHeader.replace("Bearer ", "")
         }
         return null
